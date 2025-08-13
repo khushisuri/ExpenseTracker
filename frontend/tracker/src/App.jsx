a01175d15704545aad9d5543c278906033d8ac40
khushisuri <khushimuskaan1999@gmail.com>
2025-08-12 22:07:48 -0400

    typo-fix

--- frontend/tracker/src/App.jsx
+++ frontend/tracker/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Login from "./pages/Auth/Login";
-import Signup from "./pages/Auth/Signup";
+import Signup from "./pages/Auth/SignUp";
 import Home from "./pages/Dashboard/Home";
 import Income from "./pages/Dashboard/Income";
 import Expense from "./pages/Dashboard/Expense";
