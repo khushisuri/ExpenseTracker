03c160b0c48ff61fb0842255b82099ace3e3a2c6
khushisuri <khushimuskaan1999@gmail.com>
2025-08-12 21:47:53 -0400

    deployment requirements

--- frontend/tracker/src/utils/apiPaths.js
+++ frontend/tracker/src/utils/apiPaths.js
@@ -1,27 +1,27 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_BASE_URL 
 
 export const API_PATHS = {
     AUTH:{
-        LOGIN:"http://localhost:8000/api/v1/auth/login",
-        REGISTER:"http://localhost:8000/api/v1/auth/register",
-        GET_USER_INFO:"http://localhost:8000/api/v1/auth/getUser",
+        LOGIN:`${BASE_URL}/api/v1/auth/login`,
+        REGISTER:`${BASE_URL}/api/v1/auth/register`,
+        GET_USER_INFO:`${BASE_URL}/api/v1/auth/getUser`,
     },
     DASHBOARD:{
-        GET_DATA:"http://localhost:8000/api/v1/dashboard/get",
+        GET_DATA:`${BASE_URL}/api/v1/dashboard/get`,
     },
     INCOME:{
-      ADD_INCOME:"http://localhost:8000/api/v1/income/add",
-      DELETE_INCOME:(id)=>`http://localhost:8000/api/v1/income/${id}`,
-      GET_ALL_INCOME:"http://localhost:8000/api/v1/income/get",
-      DOWNLOAD_INCOME:"http://localhost:8000/api/v1/income/downloadexcel"
+      ADD_INCOME:`${BASE_URL}/api/v1/income/add`,
+      DELETE_INCOME:(id)=>`${BASE_URL}/api/v1/income/${id}`,
+      GET_ALL_INCOME:`${BASE_URL}/api/v1/income/get`,
+      DOWNLOAD_INCOME:`${BASE_URL}/api/v1/income/downloadexcel`
     },
     EXPENSE:{
-      ADD_EXPENSE:"http://localhost:8000/api/v1/expense/add",
-      DELETE_EXPENSE:(id)=>`http://localhost:8000/api/v1/expense/${id}`,
-      GET_ALL_EXPENSE:"http://localhost:8000/api/v1/expense/get",
-      DOWNLOAD_EXPENSE:"http://localhost:8000/api/v1/expense/downloadexcel"
+      ADD_EXPENSE:`${BASE_URL}/api/v1/expense/add`,
+      DELETE_EXPENSE:(id)=>`${BASE_URL}/api/v1/expense/${id}`,
+      GET_ALL_EXPENSE:`${BASE_URL}/api/v1/expense/get`,
+      DOWNLOAD_EXPENSE:`${BASE_URL}/api/v1/expense/downloadexcel`
     },
     IMAGE:{
-        UPLOAD_IMAGE:"/api/v1/auth/upload-image"
+        UPLOAD_IMAGE:`/api/v1/auth/upload-image`
     }
 }
