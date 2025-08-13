a2c700548e4292bbf34d7ede2126f46ea60b6112
khushisuri <khushimuskaan1999@gmail.com>
2025-08-12 22:23:26 -0400

    typo fix

--- frontend/tracker/src/pages/Auth/SignUp.jsx
+++ frontend/tracker/src/pages/Auth/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import AuthLayout from "../../components/layout/AuthLayout";
-import Input from "../../components/inputs/input";
+import Input from "../../components/inputs/Input";
 import { Link, useNavigate } from "react-router-dom";
 import { isValidEmail } from "../../utils/helper";
 import ProfilePicSelector from "../../components/Custom/ProfilePicSelector";
